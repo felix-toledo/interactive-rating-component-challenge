c9a0b5d7234f66b8dff25963bd94187f013506a1
Felix Toledo <felixtoledoctes@gmail.com>
2023-08-09 22:16:07 -0300

    stilll

--- src/ThankYou.jsx
+++ src/ThankYou.jsx
@@ -3,7 +3,7 @@ import './ThankYou.css'
 export default function ThankYou({puntuation}){
      return(
           <div className="cardContainer byeContainer">
-               <div className="tyImgContainer"><img src="../public/images/illustration-thank-you.svg"></img></div>
+               <div className="tyImgContainer"><img src="../dist/illustration-thank-you.svg"></img></div>
                <span className="puntuation">You selected {puntuation} out of 5</span>
                <h1>Thank you!</h1>
                <p>We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!</p>
